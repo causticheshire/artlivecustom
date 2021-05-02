3e1c11fdd0118a55b78547f6b88acc1b043f6155
causticheshire <unicornpooprainbow@autistici.org>
2021-05-02 22:23:29 +0300

    change scripts

--- cnfghost.sh
+++ cnfghost.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+start=$(date +%M%s)
 artsh_dir=$(pwd)
 # install requirements
 pacman-key --init
@@ -37,7 +38,7 @@ else
 fi
 #change live system password
 read -r -p "Enter password for live system: " -e -i $pasw artix
-sed -i 's/# PASSWORD="artix"/PASSWORD="$pasw"/' $work_dir$profile/profile.conf
+echo 'PASSWORD="'$pasw'"' >> $work_dir$profile/profile.conf
 #change pre-installed packages
 #need more work
 sed -i 's/connman-gtk//' $work_dir$profile/Packages-Root
@@ -49,14 +50,92 @@ sleep 3
 #prebuild rootfs live system
 buildiso -p $profile
 rtfs_dir=/var/lib/artools/buildiso/$profile/artix/rootfs
+#chell audio drivers
+read -r -p "Install audio drivers HP Chromebook? - [Y/n]: " -e -i $audio n
+if [[ "$audio" == "Y" ]] || [[ "$audio" == "y" ]]; then
+    echo "Wait for installing audio drivers"
+    sleep 2
+    git clone --recurse-submodules https://github.com/causticheshire/chell_audio.git
+    cp chell_audio/galliumos-skylake/lib/firmware/* $rtfs_dir/lib/firmware
+    cp chell_audio/firmware/intel/* $rtfs_dir/lib/firmware/intel/
+    cp -r chell_audio/ucm2/* $rtfs_dir/usr/share/alsa/ucm2
+    cp -r chell_audio/galliumos-skylake/etc/* $rtfs_dir/etc
+    echo "Audio drivers successfully installed"
+    sleep 2
+else
+    echo "Drivers will not be installed"
+    sleep 2
+fi
 #install aur packages for integration to live system
 cat $artsh_dir/pkgyay.conf | sed s/' '//g | sudo --user=$SUDO_USER yay -S - --noanswerclean --noanswerdiff --noansweredit --noeditmenu --nodiffmenu --noremovemake --noconfirm 
-
-mkdir $rtfs_dir/home/pkgs
-find /home/$SUDO_USER/.cache/yay/ -name "*.zst" -exec cp '{}' $rtfs_dir/home/pkgs/ \;
-cp $artsh_dir/artin.sh $rtfs_dir
+#cp aur packages to rootfs
+mkdir $rtfs_dir/pkgs
+find /home/$SUDO_USER/.cache/yay/ -name "*.zst" -exec cp '{}' $rtfs_dir/pkgs/ \;
+#cp scripts for work in chroot
+#cp $artsh_dir/artin.sh $rtfs_dir
 cp $artsh_dir/.post $rtfs_dir
 #chroot to rootfs live system
-artix-chroot $rtfs_dir
+artix-chroot $rtfs_dir <<EOF
+pacman-key --init
+pacman-key --populate
+pacman-key --populate artix
+pacman-key --refresh-keys
+pacman -Syyu --noconfirm
+pacman -S linux-hardened fakeroot git curl wget tor-openrc qtox gcc make firefox-developer-edition electrum telegram-desktop qbittorrent veracrypt keepassxc monero-gui bleachbit dnscrypt-proxy-openrc go base-devel --noconfirm --needed
+git clone https://github.com/causticheshire/Proxybound.git
+cd Proxybound
+./configure
+make
+make install
+cp src/proxybound.conf /etc/
+cd /
+rm -rf Proxybound
+pacman -U /home/pkgs/*.zst --noconfirm --needed
+sed -i 's/socks4  127.0.0.1 9050/socks5\t192.168.8.1\t9050/' /etc/proxybound.conf
+EOF
+#build live system
+buildiso -p $profile -xc
+buildiso -p $profile -sc
+buildiso -p $profile -bc
+buildiso -p $profile -zc
+iso_dir=/home/$SUDO_USER/artools-workspace/iso/$profile
+date_now=$(date +'%Y%m%d')
+echo "Pls connect device"
+sleep 10
+read -r -p "Use ventoy? - [Y/n]: " -e -i $vtny Y
+if [[ "$vtny" == "Y" ]] || [[ "$vtny" == "y" ]]; then
+    echo "Select device - use only device name (not partition) from NAME column"
+    echo "ALL DATA WILL BE CLEARED"
+    lsblk -f
+    read -r -p "Select a disk (sdX): " -e -i $sdxx /dev/sd
+    echo "Create label for device, dnt use system names"
+    read -r -p "Create label: " -e -i $lbl ART
+    ventoy -s -g -I $sdxx -L $lbl
+    mkdir /mnt/$lbl
+    sdxa=${sdxx}1
+    mount $sdxa /mnt/$lbl
+    echo "Wait - copy iso to device"
+    sleep 2
+    rsync $iso_dir/artix-$profile-openrc-$date_now-x86_64.iso /mnt/$lbl/xfce.iso
+    sum0=$(md5sum $iso_dir/artix-$profile-openrc-$date_now-x86_64.iso | cut -c1-32)
+    sum1=$(md5sum /mnt/$lbl/xfce.iso | cut -c1-32)
+    if [[ "$sum0" == "$sum1" ]]; then
+        echo "Successful"
+        sleep 1
+    else
+        echo "Iso corrupted, pls contact with administrator"
+    fi
+else
+    echo "Select device - use only device name (not partition) from NAME column"
+    echo "ALL DATA WILL BE CLEARED"
+    lsblk -f
+    read -r -p "Select a disk (sdX): " -e -i $sdxx /dev/sd
+    echo "Writing..."
+    dd if=$iso_dir/artix-$profile-openrc-$date_now-x86_64.iso of=$sdxx bs=1M status=progress
+    echo "Successful"
+fi
+end=$(date +%M%s)
+diff=$($end - $start)
+echo $diff
 
 

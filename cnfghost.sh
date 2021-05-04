066e42269a71aaada273c48b010682bda99726e9
causticheshire <unicornpooprainbow@autistici.org>
2021-05-04 14:12:48 +0300

    change scripts

--- cnfghost.sh
+++ cnfghost.sh
@@ -1,52 +1,60 @@
 #!/bin/bash
 start=$(date +%M%s)
 artsh_dir=$(pwd)
-# install requirements
-pacman-key --init
-pacman-key --populate
-#pacman-key --populate artix
-pacman-key --refresh-keys
-pacman -Syyu --noconfirm
-modprobe loop
-pacman -S artools iso-profiles git base-devel go --needed --noconfirm
-sudo --user=$SUDO_USER git clone https://aur.archlinux.org/yay.git
-cd yay
-sudo --user=$SUDO_USER makepkg -si --noconfirm
-cd $artsh_dir
-# copy iso profiles configs
-mkdir /home/$SUDO_USER/artools-workspace
-cp -r /usr/share/artools/iso-profiles /home/$SUDO_USER/artools-workspace/
-work_dir=/home/$SUDO_USER/artools-workspace/iso-profiles/
-#select profile live system
-echo "Select ur profile:"
-select profile in "base" "cinnamon" "common" "community" "community-gtk" "community-qt" "linexa" "lxde" "lxqt" "mate" "plasma" "xfce"
-do
-    echo "U select " $profile
-    break
-done
-#change autologin live system
-read -r -p "Enable autologin - [Y/n]: " -e -i "Y" auto
-if [[ "$auto" == "Y" ]] || [[ "$auto" == "y" ]]; then
-    echo "ENABLE AUTOLOGIN"
-    sed -i 's/AUTOLOGIN="true"/AUTOLOGIN="true"/' $work_dir$profile/profile.conf
-else
-    echo "DISABLE AUTOLOGIN"
-    sed -i 's/AUTOLOGIN="true"/AUTOLOGIN="false"/' $work_dir$profile/profile.conf
-fi
-#change live system password
-read -r -p "Enter password for live system: " -e -i "artix" pasw
-echo 'PASSWORD="'$pasw'"' >> $work_dir$profile/profile.conf
-#change pre-installed packages
-#need more work
-sed -i 's/connman-gtk//' $work_dir$profile/Packages-Root
-sed -i 's/midori/qtox/' $work_dir$profile/Packages-Root
-sed -i 's/gparted/#gparted/' $work_dir$profile/Packages-Live
-#test profile
-buildiso -p $profile -q
-sleep 3 #dnt touch this sleep 3
+function prehost {
+    # install requirements
+    pacman-key --init
+    pacman-key --populate
+    #pacman-key --populate artix
+    pacman-key --refresh-keys
+    pacman -Syyu --noconfirm
+    modprobe loop
+    pacman -S artools iso-profiles git base-devel go --needed --noconfirm
+    sudo --user=$SUDO_USER git clone https://aur.archlinux.org/yay.git
+    cd yay
+    sudo --user=$SUDO_USER makepkg -si --noconfirm
+    cd $artsh_dir
+}
+function profiling {
+    # copy iso profiles configs
+    mkdir /home/$SUDO_USER/artools-workspace
+    cp -r /usr/share/artools/iso-profiles /home/$SUDO_USER/artools-workspace/
+    work_dir=/home/$SUDO_USER/artools-workspace/iso-profiles/
+    #select profile live system
+    echo "Select ur profile:"
+    select profile in "base" "cinnamon" "common" "community" "community-gtk" "community-qt" "linexa" "lxde" "lxqt" "mate" "plasma" "xfce"
+    do
+        echo "U select " $profile
+        break
+    done
+    #change autologin live system
+    read -r -p "Enable autologin - [Y/n]: " -e -i "Y" auto
+    if [[ "$auto" == "Y" ]] || [[ "$auto" == "y" ]]; then
+        echo "ENABLE AUTOLOGIN"
+        sed -i 's/AUTOLOGIN="true"/AUTOLOGIN="true"/' $work_dir$profile/profile.conf
+    else
+        echo "DISABLE AUTOLOGIN"
+        sed -i 's/AUTOLOGIN="true"/AUTOLOGIN="false"/' $work_dir$profile/profile.conf
+    fi
+    #change live system password
+    read -r -p "Enter password for live system: " -e -i "artix" pasw
+    echo 'PASSWORD="'$pasw'"' >> $work_dir$profile/profile.conf
+    #change pre-installed packages
+    #need more work
+    sed -i 's/connman-gtk//' $work_dir$profile/Packages-Root
+    sed -i 's/midori/qtox/' $work_dir$profile/Packages-Root
+    sed -i 's/gparted/#gparted/' $work_dir$profile/Packages-Live
+}
+function ptest {
+    #test profile
+    buildiso -p $profile -q
+}
+function prebuild {
 #prebuild rootfs live system
-buildiso -p $profile
-rtfs_dir=/var/lib/artools/buildiso/$profile/artix/rootfs
+    buildiso -p $profile
+    rtfs_dir=/var/lib/artools/buildiso/$profile/artix/rootfs
+}
+function pconf {
 #chell audio drivers
 read -r -p "Install audio drivers HP Chromebook? - [Y/n]: " -e -i "n" audio
 if [[ "$audio" == "Y" ]] || [[ "$audio" == "y" ]]; then
@@ -86,49 +94,52 @@ rm -rf Proxybound
 pacman -U /home/pkgs/*.zst --noconfirm --needed
 sed -i 's/socks4  127.0.0.1 9050/socks5\t192.168.8.1\t9050/' /etc/proxybound.conf
 EOF
-#build live system
-buildiso -p $profile -xc
-buildiso -p $profile -sc
-buildiso -p $profile -bc
-buildiso -p $profile -zc
-iso_dir=/home/$SUDO_USER/artools-workspace/iso/$profile
-date_now=$(date +'%Y%m%d')
-echo "Pls connect device"
-sleep 3 #dnt touch this sleep 3
-read -n 1 -s -r -p "Press any button to continue"
-read -r -p "Use ventoy? - [Y/n]: " -e -i "Y" vtny
-if [[ "$vtny" == "Y" ]] || [[ "$vtny" == "y" ]]; then
-    echo "Select device - use only device name (not partition) from NAME column"
-    echo "ALL DATA WILL BE CLEARED"
-    lsblk -f
-    read -r -p "Select a disk (sdX): " -e -i "/dev/sd" sdxx
-    echo "Create label for device, dnt use system names"
-    read -r -p "Create label: " -e -i "ART" lbl
-    ventoy -s -g -I $sdxx -L $lbl
-    mkdir /mnt/$lbl
-    sdxa=${sdxx}1
-    mount $sdxa /mnt/$lbl
-    echo "Wait - copy iso to device"
-    rsync $iso_dir/artix-$profile-openrc-$date_now-x86_64.iso /mnt/$lbl/xfce.iso
-    sum0=$(md5sum $iso_dir/artix-$profile-openrc-$date_now-x86_64.iso | cut -c1-32)
-    sum1=$(md5sum /mnt/$lbl/xfce.iso | cut -c1-32)
-    if [[ "$sum0" == "$sum1" ]]; then
-        umount $sdxa
-        eject $sdxx
-        echo "Successful"
+}
+function isobuild {
+    #build live system
+    buildiso -p $profile -xc
+    buildiso -p $profile -sc
+    buildiso -p $profile -bc
+    buildiso -p $profile -zc
+    iso_dir=/home/$SUDO_USER/artools-workspace/iso/$profile
+}
+function isowrite {
+    local date_now=$(date +'%Y%m%d')
+    read -n 1 -s -r -p "Pls connect device and press any button to continue"
+    read -r -p "Use ventoy? - [Y/n]: " -e -i "Y" vtny
+    if [[ "$vtny" == "Y" ]] || [[ "$vtny" == "y" ]]; then
+        echo "Select device - use only device name (not partition) from NAME column"
+        echo "ALL DATA WILL BE CLEARED"
+        lsblk -f
+        read -r -p "Select a disk (sdX): " -e -i "/dev/sd" sdxx
+        echo "Create label for device, dnt use system names"
+        read -r -p "Create label: " -e -i "ART" lbl
+        ventoy -s -g -I $sdxx -L $lbl
+        mkdir /mnt/$lbl
+        local sdxa=${sdxx}1
+        mount $sdxa /mnt/$lbl
+        echo "Wait - copy iso to device"
+        rsync $iso_dir/artix-$profile-openrc-$date_now-x86_64.iso /mnt/$lbl/xfce.iso
+        local sum0=$(md5sum $iso_dir/artix-$profile-openrc-$date_now-x86_64.iso | cut -c1-32)
+        local sum1=$(md5sum /mnt/$lbl/xfce.iso | cut -c1-32)
+        if [[ "$sum0" == "$sum1" ]]; then
+            umount $sdxa
+            eject $sdxx
+            echo "Successful"
+        else
+            echo "Iso corrupted, pls contact with administrator"
+        fi
     else
-        echo "Iso corrupted, pls contact with administrator"
+        echo "Select device - use only device name (not partition) from NAME column"
+        echo "ALL DATA WILL BE CLEARED"
+        lsblk -f
+        read -r -p "Select a disk (sdX): " -e -i "/dev/sd" sdxx
+        echo "Writing..."
+        dd if=$iso_dir/artix-$profile-openrc-$date_now-x86_64.iso of=$sdxx bs=1M status=progress
+        echo "Successful"
+        eject $sdxx
     fi
-else
-    echo "Select device - use only device name (not partition) from NAME column"
-    echo "ALL DATA WILL BE CLEARED"
-    lsblk -f
-    read -r -p "Select a disk (sdX): " -e -i "/dev/sd" sdxx
-    echo "Writing..."
-    dd if=$iso_dir/artix-$profile-openrc-$date_now-x86_64.iso of=$sdxx bs=1M status=progress
-    echo "Successful"
-    eject $sdxx
-fi
+}
 end=$(date +%M%s)
 diff=$($end - $start)
 echo $diff
